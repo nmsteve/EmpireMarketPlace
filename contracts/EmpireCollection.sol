1ad7e4269d2fdf4bf17db2f3050d8ee81406af21
nmsteve <nmsteve45@gmail.com>
2022-06-30 23:04:10 +0300

    add modifier:OnlyOwnerOrdeployer

--- contracts/EmpireCollection.sol
+++ contracts/EmpireCollection.sol
@@ -21,24 +21,37 @@ contract EmpireCollection is ERC721, Ownable, ERC721Royalty, IEmpireCollection{
     
     string public baseURI;
     uint256 public _totalSupply;
+    bool public revealed = false;
+    address public deployer;
+    
+
     
 
     event LogChangeBaseURI(string _baseURI);
     event LogWithdraw(address account, uint256 amount);
     event LogWithdrawERC20(address account, uint256 amount, address token);
 
+    modifier onlyOwnerOrDeployer() {
+        require(owner() == _msgSender() || deployer == _msgSender(), "Ownable: caller is not the owner or deployer");
+        _;
+    }
 
-    constructor(string memory baseURI_, string memory name, string memory ticker) ERC721(name, ticker) {
+
+    constructor(string memory baseURI_, string memory name, string memory ticker, address _deployer) ERC721(name, ticker) {
         // Set contract URI
         baseURI = baseURI_;
-        //transferOwnership(owner);
+        deployer = _deployer;
     }
-     
+
+    function changeRevealed(bool _revealed) public onlyOwnerOrDeployer {
+        revealed = _revealed;
+    }
+    
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
 
-    function changeBaseURI(string memory baseURI_) public onlyOwner {
+    function changeBaseURI(string memory baseURI_) public onlyOwnerOrDeployer {
         baseURI = baseURI_;
         emit LogChangeBaseURI(baseURI_);
     }
@@ -47,7 +60,7 @@ contract EmpireCollection is ERC721, Ownable, ERC721Royalty, IEmpireCollection{
         return address(this);
     }
     
-    function mint(uint256 num, address token, uint256 mintPriceInWei, address user, uint96 royalties) override public onlyOwner{
+    function mint(uint256 num, address token, uint256 mintPriceInWei, address user, uint96 royalties) override public onlyOwnerOrDeployer{
         uint256 amount = num * mintPriceInWei;
         require(IERC20(token).allowance(user, address(this)) >= amount, "Insufficient allowance.");
         require(IERC20(token).balanceOf(user) >= amount, "Insufficient balance.");
@@ -63,7 +76,7 @@ contract EmpireCollection is ERC721, Ownable, ERC721Royalty, IEmpireCollection{
         IERC20(token).transferFrom(user, owner(), amount);
     }
 
-    function mintBNB(uint256 num, uint256 mintPriceInWei, address user, uint96 royalties) public override payable onlyOwner{
+    function mintBNB(uint256 num, uint256 mintPriceInWei, address user, uint96 royalties) public override payable onlyOwnerOrDeployer{
         uint256 amount = num * mintPriceInWei;
         require(msg.value >= amount, "Not enough bnb sent");
         require(royalties <= 1000, "Max Royalties 10%");
@@ -76,21 +89,26 @@ contract EmpireCollection is ERC721, Ownable, ERC721Royalty, IEmpireCollection{
         }
 
     }
-
+    
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
         string memory baseURI_ = _baseURI();
-        return bytes(baseURI_).length > 0 ? string(abi.encodePacked(baseURI_, Strings.toString(tokenId), ".json")) : "";
-        
+
+        if (revealed) {
+            return bytes(baseURI_).length > 0 ? string(abi.encodePacked(baseURI_, Strings.toString(tokenId), ".json")) : "";
+        } else {
+            return string(abi.encodePacked(baseURI_, "empirehidden.json"));
+        }
     }
 
-    function withdraw(address payable account, uint256 amount) external onlyOwner {
+    function withdraw(address payable account, uint256 amount) external onlyOwnerOrDeployer{
         require(amount <= address(this).balance, "Insuficcient funds");
         account.transfer(amount);
         emit LogWithdraw(account, amount);
     }
 
-    function withdrawERC20(address account, uint256 amount, address token) external onlyOwner {
+    function withdrawERC20(address account, uint256 amount, address token) external onlyOwnerOrDeployer{
         require(amount <= IERC20(token).balanceOf(address(this)), "Insuficcient funds");
         IERC20(token).transfer(account, amount);
         emit LogWithdrawERC20(account, amount, token);
@@ -134,10 +152,10 @@ contract CollectionMinter is Ownable {
         emit LogSetTresuary(_tresuary);
     }
     
-    function createNewCollection(string memory name, string memory symbol, string memory _baseURI) external{
+    function createNewCollection(string memory _baseURI,string memory name, string memory symbol,  address owner) external{
         _collectionIds.increment();
         uint256 newCollectionId = _collectionIds.current();
-        EmpireCollection collection = new EmpireCollection(_baseURI,name,symbol);
+        EmpireCollection collection = new EmpireCollection(_baseURI,name,symbol, owner);
         address collectionAddress = collection.getAddress();
         userCollection[msg.sender][newCollectionId] = collectionAddress;
         ownerOf[newCollectionId] = msg.sender;
